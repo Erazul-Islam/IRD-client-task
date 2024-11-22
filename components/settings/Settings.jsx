ca756df313e517c477e4307ba50f617f6952286f
Erazul Islam <erazul89@gmail.com>
2024-11-22 19:42:23 +0600

    make settings interactive

--- components/settings/Settings.jsx
+++ components/settings/Settings.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable prettier/prettier */
 import React from 'react';
 
@@ -17,30 +19,27 @@ const Settings = () => {
                     </div>
                 </div>
                 <div className='flex'>
-                    <div className='bg-[#1FA45B] w-[3px] h-[50px] rounded-xl'>
-                    </div>
+                    
                     <div className='bg-[#E8F0F5] mt-2 w-9 h-9 rounded-full  ml-6 '>
-                        <img className='mt-2 ml-[5px]' src="https://i.ibb.co.com/Hg2H9FD/general.png" alt="" />
+                        <img className='mt-[5px] ml-[5px]' src="https://i.ibb.co.com/Hg2H9FD/general.png" alt="" />
                     </div>
                     <div>
                         <h1 className='text-[#868686] ml-6 mt-[14px]'>General Settings</h1>
                     </div>
                 </div>
                 <div className='flex'>
-                    <div className='bg-[#1FA45B] w-[3px] h-[50px] rounded-xl'>
-                    </div>
+                    
                     <div className='bg-[#E8F0F5] mt-2 w-9 h-9 rounded-full  ml-6 '>
-                        <img className='mt-2 ml-[5px]' src="https://i.ibb.co.com/GQbmBXQ/ruqyah.png" alt="" />
+                        <img className='mt-[8px] ml-[7px]' src="https://i.ibb.co.com/GQbmBXQ/ruqyah.png" alt="" />
                     </div>
                     <div>
                         <h1 className='text-[#868686] ml-6 mt-[14px]'>Font Settings</h1>
                     </div>
                 </div>
                 <div className='flex'>
-                    <div className='bg-[#1FA45B] w-[3px] h-[50px] rounded-xl'>
-                    </div>
+                   
                     <div className='bg-[#E8F0F5] mt-2 w-9 h-9 rounded-full  ml-6 '>
-                        <img className='mt-2 ml-[5px]' src="https://i.ibb.co.com/8KKCzsD/books.png" alt="" />
+                        <img className='mt-[8px] ml-[7px]' src="https://i.ibb.co.com/8KKCzsD/books.png" alt="" />
                     </div>
                     <div>
                         <h1 className='text-[#868686] ml-6 mt-[14px]'>Appearence Settings</h1>
